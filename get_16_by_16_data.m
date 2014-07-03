715718f6dc8f2bc3e211a2a8630d8b765c12b87e
mercher <jjsm76@gmail.com>
2014-07-03 22:56:57 +0100

    Fixed file path bug
    Now concatenates specified file path to filename when loading data file

--- get_16_by_16_data.m
+++ get_16_by_16_data.m
@@ -2,7 +2,7 @@
 function [training_images, training_labels, validation_images, validation_labels, test_images, test_labels]  = get_16_by_16_data(	data_filename = 'data16.mat', ...
 																																						filepath		 = 'C:\Users\jjsm\Documents\MNIST NN project\' )																
 	# Load images and labels (note that this is a struct with everything folded up into it)
-	images = load( data_filename );
+	images = load( strcat( filepath, data_filename ) );
 
 	training_images 		= images.data.training.inputs;
 	validation_images 	= images.data.validation.inputs;
